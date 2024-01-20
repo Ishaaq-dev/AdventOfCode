46f64a49ea1b0f9c2fae2e1094bc90075e4ef9f9
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-01-20 18:46:53 +0000

    adds correct answer unit test

--- 2023/Day_3/tests/index.test.js
+++ 2023/Day_3/tests/index.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import { expect } from "chai";
 import { getSumOfPartNumbers } from "../src/index.js";
 import { Scenario } from "./testData.js";
@@ -5,6 +6,13 @@ import { Scenario } from "./testData.js";
 describe('getSumOfPartnumbers()', () => {
     it('should return 4361', () => {
         const input = Scenario.full, expectedResult = 4361;
+        console.log(input);
+        const result = getSumOfPartNumbers(input);
+        expect(result).to.equal(expectedResult);
+    });
+    it('should return 544664', () => {
+        const gearRatioData = fs.readFileSync('./data/input.txt', 'utf-8').split('\n');
+        const input = gearRatioData, expectedResult = 544664;
         console.log(input);
         const result = getSumOfPartNumbers(input);
         expect(result).to.equal(expectedResult);
