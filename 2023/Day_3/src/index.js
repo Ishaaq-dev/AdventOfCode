7b652fab5e9c537ceaf1a8a62cc0cab1067efcb1
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-02-13 17:46:25 +0000

    modifies result

--- 2023/Day_3/src/index.js
+++ 2023/Day_3/src/index.js
@@ -1,25 +1,36 @@
 import fs from 'fs';
-import { getSumOfEngineSchematic, getSymbolIndexes } from './GearRatios.js';
+import { getAsterixIndexObj, getMultipliedGearRatios, getGearsFromEngineSchematic, getSumOfEngineSchematic, getSymbolIndexes } from './GearRatios.js';
 
 const gearRatioData = fs.readFileSync('./data/input.txt', 'utf-8').split('\n');
 
 export function getSumOfPartNumbers(data) {
-    const partNumberSums = [];
+    const partNumberSums = [], gearNumberSums = [];
     for (let i = 0; i < data.length; i++) {
         const limit = i + 1;
         let j;
         if (i - 1 < 0) j = 0;
         else j = i - 1;
 
+        let asterixIndexObj = getAsterixIndexObj(data[i]);
+
         for (j; j <= limit; j++ ) {
-            const value = data[j];
-            const symbolIndexes = getSymbolIndexes(value);
-            if (!symbolIndexes.length) continue;
-            const partNumbersSum = getSumOfEngineSchematic(data[i], symbolIndexes);
-            if (partNumbersSum) partNumberSums.push(partNumbersSum);
+            const intermittenLine = data[j];
+            const symbolIndexes = getSymbolIndexes(intermittenLine);
+            if (symbolIndexes.length) {
+                const partNumbersSum = getSumOfEngineSchematic(data[i], symbolIndexes);
+                if (partNumbersSum) partNumberSums.push(partNumbersSum);
+            }
+            asterixIndexObj = getGearsFromEngineSchematic(intermittenLine, asterixIndexObj);
         }
+        const multipliedGearRatios = getMultipliedGearRatios(asterixIndexObj);
+        if (multipliedGearRatios > 0)
+            gearNumberSums.push(multipliedGearRatios);
     }
-    return partNumberSums.reduce((acc, cur) => acc += cur, 0);
+    console.log('gearNumberSums: ', gearNumberSums);
+    return {
+        partNum: partNumberSums.reduce((acc, cur) => acc += cur, 0), 
+        gearNum: gearNumberSums.reduce((acc, cur) => acc += cur, 0)
+    }
 }
 
 console.log('Sum of part numbers', getSumOfPartNumbers(gearRatioData));
--- 2023/Day_3/tests/index.test.js
+++ 2023/Day_3/tests/index.test.js
@@ -4,27 +4,37 @@ import { getSumOfPartNumbers } from "../src/index.js";
 import { Scenario } from "./testData.js";
 
 describe('getSumOfPartnumbers()', () => {
-    it('should return 4361', () => {
-        const input = Scenario.full, expectedResult = 4361;
-        const result = getSumOfPartNumbers(input);
-        expect(result).to.equal(expectedResult);
+    describe('PART I', () => {
+        it('should return 4361', () => {
+            const input = Scenario.full, expectedResult = 4361;
+            const result = getSumOfPartNumbers(input);
+            expect(result.partNum).to.equal(expectedResult);
+        });
+        it('should return 940', () => {
+            const input = Scenario.J, expectedResult = 940;
+            const result = getSumOfPartNumbers(input);
+    
+            expect(result.partNum).to.equal(expectedResult);
+        });
+        it('should return 544664', () => {
+            const gearRatioData = fs.readFileSync('./data/input.txt', 'utf-8').split('\n');
+            const input = gearRatioData, expectedResult = 544664;
+            const result = getSumOfPartNumbers(input);
+            expect(expectedResult - result.partNum).to.equal(0);
+            expect(result.partNum).to.equal(expectedResult);
+        });
     });
-    it('should return 940', () => {
-        const input = Scenario.J, expectedResult = 940;
-        const result = getSumOfPartNumbers(input);
-
-        expect(result).to.equal(expectedResult);
-    });
-    it('should return 544664', () => {
-        const gearRatioData = fs.readFileSync('./data/input.txt', 'utf-8').split('\n');
-        const input = gearRatioData, expectedResult = 544664;
-        const result = getSumOfPartNumbers(input);
-        expect(expectedResult - result).to.equal(0);
-        expect(result).to.equal(expectedResult);
-    });
-    it('Part II - should return 467835', () => {
-        const input = Scenario.full, expectedResult = 467835;
-        const result = getSumOfPartNumbers(input);
-        expect(result).to.equal(expectedResult); 
+    describe('Part II', () => {
+        it('should return 467835', () => {
+            const input = Scenario.full, expectedResult = 467835;
+            const result = getSumOfPartNumbers(input);
+            expect(result.gearNum).to.equal(expectedResult); 
+        });
+        it('should return 11663', () => {
+            const input = Scenario.J, expectedResult = 11663;
+            const result = getSumOfPartNumbers(input);
+    
+            expect(result.gearNum).to.equal(expectedResult);
+        });
     });
 });
