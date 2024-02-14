2be27e3e4b5b31763135ac537ab4987f444b6bd6
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-02-14 15:31:09 +0000

    creates functions to return cards as arrays of ints

---
+++ 2023/Day_4/Src/helpers.js
@@ -0,0 +1,20 @@
+export function getWinningAndPlayingNums(card) {
+    const cardSplit = card.split(':');
+    const numbersSplit = cardSplit[1].split('|');
+
+    const winningNums = convertStringNumbersToIntArray(numbersSplit[0]);
+    const playingNums = convertStringNumbersToIntArray(numbersSplit[1]);
+
+    return { winningNums, playingNums }
+}
+
+export function convertStringNumbersToIntArray(stringNumbers) {
+    const intNumbersArray = [];
+    const stringNumbersArray = stringNumbers.trim().split(' ');
+    stringNumbersArray.forEach(stringNumber => {
+        const intNumber = parseInt(stringNumber);;
+        if(!isNaN(intNumber)) intNumbersArray.push(intNumber);
+    });
+
+    return intNumbersArray;
+}
---
+++ 2023/Day_4/Tests/helpers.test.js
@@ -0,0 +1,82 @@
+import { expect } from "chai";
+import { convertStringNumbersToIntArray, getWinningAndPlayingNums } from "../Src/helpers.js";
+import { Cards } from "./testData.js";
+
+describe('getWinningAndPlayingNums()', () => {
+    it('card One', () => {
+        const input = Cards.One, expectedResult = {
+            winningNums: [41,48,83,86,17],
+            playingNums: [83,86,6,31,17,9,48,53]
+        };
+        const result = getWinningAndPlayingNums(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Two', () => {
+        const input = Cards.Two, expectedResult = {
+            winningNums: [13,32,20,16,61],
+            playingNums: [61,30,68,82,17,32,24,19]
+        };
+        const result = getWinningAndPlayingNums(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Three', () => {
+        const input = Cards.Three, expectedResult = {
+            winningNums: [1,21,53,59,44],
+            playingNums: [69,82,63,72,16,21,14,1]
+        };
+        const result = getWinningAndPlayingNums(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Four', () => {
+        const input = Cards.Four, expectedResult = {
+            winningNums: [41,92,73,84,69],
+            playingNums: [59,84,76,51,58,5,54,83]
+        };
+        const result = getWinningAndPlayingNums(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Five', () => {
+        const input = Cards.Five, expectedResult = {
+            winningNums: [87,83,26,28,32],
+            playingNums: [88,30,70,12,93,22,82,36]
+        };
+        const result = getWinningAndPlayingNums(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Six', () => {
+        const input = Cards.Six, expectedResult = {
+            winningNums: [31,18,13,56,72],
+            playingNums: [74,77,10,23,35,67,36,11]
+        };
+        const result = getWinningAndPlayingNums(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+});
+
+describe('convertStringNumbersToIntArray()', () => {
+    it('card One: winning nums', () => {
+        const input = ' 41 48 83 86 17', expectedResult = [41,48,83,86,17];
+        const result = convertStringNumbersToIntArray(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card One: playing nums', () => {
+        const input = ' 83 86  6 31 17  9 48 53', expectedResult = [83,86,6,31,17,9,48,53];
+        const result = convertStringNumbersToIntArray(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Two: winning nums', () => {
+        const input = " 13 32 20 16 61 ", expectedResult = [13,32,20,16,61];
+        const result = convertStringNumbersToIntArray(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card Two: playing nums', () => {
+        const input = " 61 30 68 82 17 32 24 19 ", expectedResult = [61,30,68,82,17,32,24,19];
+        const result = convertStringNumbersToIntArray(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+    it('card mixed: given alphanumeric string -> only returns numeric values as ints', () => {
+        const input = " 61 fg kop]k eofpe j[jf po wa[jjfie ahfuh huref ua    jifoeh a   hriuhg 0 00 30 68 82 17 32 24 19 ", expectedResult = [61,0,0,30,68,82,17,32,24,19];
+        const result = convertStringNumbersToIntArray(input);
+        expect(result).to.deep.equal(expectedResult);
+    });
+});
