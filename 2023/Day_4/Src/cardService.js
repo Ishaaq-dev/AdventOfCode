cffa7008dc9f902686463f2429f19621e808b79d
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-02-14 15:52:24 +0000

    completes functionality to successfully return correct value for readme sample

--- 2023/Day_4/Src/cardService.js
+++ 2023/Day_4/Src/cardService.js
@@ -1,5 +1,19 @@
+import { getWinningAndPlayingNums } from "./helpers.js";
+
 export function getTotalPointsFromCard(card) {
-    let totalPoints = 0;
+    let totalPoints = 1;
+    const numbers = getWinningAndPlayingNums(card);
+
+    let quantityOfWinningNums = 0;
+    numbers.winningNums.forEach(winningNum => {
+        if(numbers.playingNums.includes(winningNum)) quantityOfWinningNums++;
+    });
+
+    if (quantityOfWinningNums <= 1) return quantityOfWinningNums;
+
+    for (let i = 1; i < quantityOfWinningNums; i++) {
+        totalPoints *= 2;
+    }
 
     return totalPoints;
 }
