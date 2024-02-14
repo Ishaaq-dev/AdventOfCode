e5ace36fe7d690b62ef3d0a8af76f6666689bd44
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-02-14 17:23:20 +0000

    reorders var initialisation in getTotalPointsFromCard()

--- 2023/Day_4/Src/cardService.js
+++ 2023/Day_4/Src/cardService.js
@@ -1,12 +1,12 @@
 import { getQuantityOfWinningNums, getCardDetails } from "./helpers.js";
 
 export function getTotalPointsFromCard(card) {
-    let totalPoints = 1;
     const numbers = getCardDetails(card);
 
     const quantityOfWinningNums = getQuantityOfWinningNums(numbers.winningNums, numbers.playingNums);
 
     if (quantityOfWinningNums <= 1) return quantityOfWinningNums;
+    let totalPoints = 1;
     for (let i = 1; i < quantityOfWinningNums; i++) {
         totalPoints *= 2;
     }
