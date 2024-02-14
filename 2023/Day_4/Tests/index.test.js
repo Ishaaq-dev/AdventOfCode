20bbfbf94df2a0d566319c31e2ab2af9cb01b689
Ishaaq-dev <i.iqbalwork@gmail.com>
2024-02-14 15:57:15 +0000

    adds unit test for whole data txt file

--- 2023/Day_4/Tests/index.test.js
+++ 2023/Day_4/Tests/index.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { expect } from "chai";
 import { getSumOfAllCards } from "../Src/index.js";
 import { Cards } from "./testData.js";
@@ -6,6 +7,11 @@ describe('getSumOfAllCards()', () => {
     describe('Part I', () => {
         it('should return 13', () => {
             const input = Cards.All, expectedResult = 13;
+            const result = getSumOfAllCards(input);
+            expect(result).to.equal(expectedResult);
+        });
+        it('should return 22193', () => {
+            const input = fs.readFileSync('./Data/input.txt', 'utf-8').split('\n'), expectedResult = 22193;
             const result = getSumOfAllCards(input);
             expect(result).to.equal(expectedResult);
         });
